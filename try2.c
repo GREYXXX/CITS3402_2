04170cf43db17c9a8c2a83503346375f78c24319
GREYXXX <33743607+GREYXXX@users.noreply.github.com>
2019-10-12 18:29:48 +0800

    Add files via upload

--- try2.c
+++ try2.c
@@ -1,37 +1,50 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include "variables.h"
 
-int main(int argc, char *argv[])
+graph readfile(char *arg)
 {       
         int got1,got2;
-        FILE *fp = fopen(argv[1],"r");
+	Graph g;
+        FILE *fp = fopen(arg,"r");
         if(fp == NULL) {
                 perror("Unable to open file!");
                 exit(1);
         }
-        //fseek(fp,0,SEEK_END);
-        //int size = ftell(fp);
+       
         int *chunk = malloc(sizeof(int));
         int k = 0;
         int l;
 	got1 = fread(chunk, sizeof(int), 1, fp);
+	g.vexnum = *chunk;
 
 	int count = ((*chunk)*(*chunk));
 	printf("count is %d\n",count);
-	//printf("file is %d\n",size);
 	int *buffer = malloc(sizeof(int)*(count));
 	got2 = fread(buffer, sizeof(int), count, fp);
-	//l = k;
 	printf("chunk is %d\n",*chunk);
+	g.matrix = int(**)malloc(g.vexnum * sizeof(int*));
+	for(int k = 0;k < g.vexnum;k++){
+		g.matrix[k] = (int*)malloc(g.vexnum * sizeof(int));
+	}
+	int n = 0;
+	for(int i = 0;i < g.vexnum;i++){
+		for(int j = 0;j < g.vexnum;j++){
+			g.matrix[i][j] = buffer[n];
+			n++;
+		}
+	}
+/*			
 	for (int i = 0; i < count; ++i) {
                 printf("buffer[%d] = %d\n", i, buffer[i]);
     }
-
+*/
         //printf("size is %d\n", buffer[count]);
 	fclose(fp);
-	return 0;
 	free(chunk);
 	free(buffer);
+	return g;
+	for(int k = 0;k < g.vexnum;k++){
+                free(g.matrix[k]);
+        }
+	free(g.matrix);
 }
 
