d359902f2697d61453f855e29cd82d761a921ebc
Syukri Zainal Abidin <21972786@ecm.uwa.edu.au>
2019-10-23 14:40:45 +0800

    Final stages

--- test.c
+++ test.c
@@ -1,3 +1,8 @@
+/*
+NAME:	Syukri Zainal Abidin	Student ID:	21972786
+	Gre					22222222
+
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,27 +13,28 @@
 
 int main(int argc, char** argv) {
 
+	//Checks arguments
 	if(argc != 2) {
 		printf("Usage is mpirun -n [number of processes] %s [filename]\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 
+	//Initialise COMM rank & size
 	int size,
 	rank;
-	
+
+	//Number of vertices	
 	int *vertexNum;
+	
+	//Timer definitions
 	clock_t start_time, end_time;
 
-	MPI_File fh;
-	MPI_Status status;
-        MPI_Init(&argc, &argv);
+	MPI_File fh;				//MPI File handle
+        MPI_Init(&argc, &argv);			
         MPI_Comm_size(MPI_COMM_WORLD, &size);
         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	char processor_name[MPI_MAX_PROCESSOR_NAME];
-	int name_len;
-	MPI_Get_processor_name(processor_name, &name_len);
-
+	//MPI_File_open reads the provided file
 	int rc = MPI_File_open(MPI_COMM_WORLD, argv[1], MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
 	if(rc) {
 		if(rank == 0) {
@@ -39,19 +45,23 @@ int main(int argc, char** argv) {
 	}
 
 	vertexNum = malloc(sizeof(int));
+	//Root process reads the first element of the file which holds the number of vertices
 	if(rank == 0) {
 		MPI_File_read_at(fh, 0, vertexNum, 1, MPI_INT, MPI_STATUS_IGNORE);
-		printf("Number of vertices is %d\n", *vertexNum);
+		//printf("Number of vertices is %d\n", *vertexNum);
 	}
+
+	//vertexNum is broadcasted to all processes
 	MPI_Bcast(vertexNum, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-	int processRow = (*vertexNum) / size; // Number of rows that each process deals with
-	int count = processRow * (*vertexNum); //Number of elements each process deals with
-	int offset = (count * sizeof(int) * rank) + 4; //File position each process starts at
+	int processRow = (*vertexNum) / size; 		//Number of rows that each process deals with
+	int count = processRow * (*vertexNum); 		//Number of elements each process deals with
+	int offset = (count * sizeof(int) * rank) + 4; 	//File position each process starts at
 
+	//Buffer stores the all the elements read by each process
 	int (*buffer)[*vertexNum] = malloc(sizeof *buffer * processRow);
 
-	printf("Process %d ready with offset: %d and will process %d rows, No. vertices: %d\n", rank, offset, processRow, *vertexNum);
+	//printf("Process %d ready with offset: %d and will process %d rows, No. vertices: %d\n", rank, offset, processRow, *vertexNum);
 
 	//Each process reads the portion of the file that it is allocated
 	MPI_File_read_at_all(fh, offset, &(buffer[0][0]), count, MPI_INT, MPI_STATUS_IGNORE);
@@ -60,6 +70,7 @@ int main(int argc, char** argv) {
 	int (*matrix)[*vertexNum];
 	matrix = malloc(sizeof *matrix * *vertexNum);
 
+	//MPI_Gather gathers all the buffers together into a single matrix
 	MPI_Gather(&(buffer[0][0]), count, MPI_INT, &(matrix[0][0]), count, MPI_INT, 0, MPI_COMM_WORLD);
 
 /*	if(rank == 0) {	
@@ -69,15 +80,17 @@ int main(int argc, char** argv) {
 		}
 	}
 	}*/
+
+	//Timer to record length of time for Floyd-Warshall algorithm
 	if(rank == 0) {
 		start_time = clock();
 	}
 
 	for(int k = 0; k < *vertexNum; k++) { 	//k is both the intermediate vertex and the process id
+		//The matrix is broadcasted to all processes
 		MPI_Bcast(&(matrix[0][0]), (*vertexNum)*(*vertexNum), MPI_INT, 0, MPI_COMM_WORLD);	
 		int start = processRow* rank;
 		int end = processRow * (rank + 1);
-		//printf("K: %d Processor %s rank %d/%d will deal with row %d - %d\n",k,  processor_name, rank, size, start, end);
 
 		for(int i = start; i < end; i++) { //ROW
 			for(int j = 0; j < *vertexNum; j++) { //COLUMN
@@ -90,7 +103,7 @@ int main(int argc, char** argv) {
 				}
 			}
 		}
-
+		//Each process sends back the portion of the matrix that it worked on to the root process
 		MPI_Gather(&(matrix[start][0]), count, MPI_INT, &(matrix[0][0]), count, MPI_INT, 0, MPI_COMM_WORLD);
 	}
 
